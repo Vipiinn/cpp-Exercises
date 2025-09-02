ae68792c3e9e0908ff8c55a140bcb5fb05eeeda0
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2025-09-02 19:56:42 +0530

    Add files via upload

---
+++ 2D Array/basics.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int arr[3][4];
+
+    //taking input
+    cout<<"Enter elements : "<<endl;
+    for(int row=0;row<3;row++){
+        for(int col=0;col<4;col++){
+            cin>>arr[row][col];
+        }
+    }
+
+    //print output
+    cout<<"output is : "<<endl;
+    for(int row=0;row<3;row++){
+        for(int col=0;col<4;col++){
+            cout<<arr[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+}
